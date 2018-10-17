16e3399e779bfd88fc3f80f0c10a40a898b6ca39
Cristian Lucero and James Cho <james@>
2018-10-17 13:08:27 -0700

    add select dropdown, styling

--- appointments/js/scripts.js
+++ appointments/js/scripts.js
@@ -5,12 +5,15 @@ $(document).ready(function() {
           var date= $("input#date").val();
           var startTime = $("input#startTime").val();
           var endTime = $("input#endTime").val();
-  
+          var language = $("select#language").val();
+        
           $("#nameOutput").append(name);
           $("#descriptionOutput").append(description);
+          $("#languageOutput").append(language);
           $("#dateOutput").append(date);
           $("#startOutput").append(startTime);
           $("#endOutput").append(endTime);
+          
   
       $("#details").fadeIn();
       event.preventDefault();
